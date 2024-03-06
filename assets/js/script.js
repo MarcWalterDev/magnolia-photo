452eea4fba1aa5d5690b985e77108ab5fa874a76
Marc Walter <marcwalter0@gmail.com>
2024-03-06 14:28:15 +0000

    updated side nav to slide in/out

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var display = 0;
 
 function hideShow() {
     if (display == 1) {
+        burger.style.right = 0;
         burger.style.display = "block";
         display = 0;
     } else {
+        buger.style.right = -100%;
         burger.style.display = "none";
         display = 1;
     }
