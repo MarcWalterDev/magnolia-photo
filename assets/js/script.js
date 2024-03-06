ec4511087ba92fece0bf986ca49c553e6848be21
Marc Walter <marcwalter0@gmail.com>
2024-03-06 23:10:23 +0000

    fixed an issue with nav not opening on 1st press on smaller screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-var burger = document.getElementById("custom-nav");
-var display = 0;
-
-function hideShow() {
-    if (display == 1) {
-        burger.style.right = 0;
-        display = 0;
-    } else {
-        burger.style.right = '-300px';
-        display = 1;
-    }
+function show() {
+	document.getElementById("custom-nav").classList.toggle("active-nav");
 }
